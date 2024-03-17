d639eae77c13c822cb550cc978f0d1cd05e26778
Abdulaziz Abdi <amigf76@gmail.com>
2024-03-17 23:40:22 +0300

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Mike", "Amy", "John"];
+const messages = [];
+
+function writeCards(names, eventName){
+    for (let a = 0; a < names.length; a++){
+       messages[a]= `Thank you, ${names[a]}, for the wonderful ${eventName} gift!`;
+       
+    
+    }
+    return messages;
+}
+
+
+let countingStart = 15;
+function countDown(countingStart){
+
+    while (countingStart >= 0){
+        console.log(countingStart--)
+
+    }
+
+
+}
